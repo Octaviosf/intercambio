ff785b7142a1dbe0b8e4a6d50deb773d284f68e3
octavio <ososaaf@gmail.com>
2019-12-25 20:37:10 -0600

    comment random split of df

---
+++ random_pair.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from members_list import members_list
+
+# test list
+members = members_list()
+
+# split list randomly by sorting by random number
+n_members = members.shape()[0] 
+
