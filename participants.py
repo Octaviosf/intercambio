5c4f58f02bc61607dd005cea2e70c64876b45553
octavio <ososaaf@gmail.com>
2019-12-25 18:12:46 -0600

    add if-else for list-file existence

--- participants.py
+++ participants.py
@@ -2,6 +2,28 @@
 Create list of participants
 """
 import pandas as pd
+import os.path 
+
+def file_exists = existence(file_path):
+	"""
+	checks for existence of file
+	"""
+
+	file_exists = os.path.isfile()
+	return file_exists
+
+# check for existence of participants list
+file_exists = input('Does participants file exist? (y or n): ')
+if file_exists:
+	path = input('Enter filepath: ')
+	participants = pd.read_csv(path)
+else:
+	participants = init_list()
+
+filename = input('Enter filename of participants list
+file_exists = existence('~/programs/python/intercambio/participants.csv')
+if file_exists:
+	participants = pd.read_csv(
 
 # Initialize via user input
 fam = []
