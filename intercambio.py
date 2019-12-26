ab799a602b19f9f14c1da004aea0821de03a8e2c
octavio <ososaaf@gmail.com>
2019-12-25 23:16:22 -0600

    add giver revealer

--- intercambio.py
+++ intercambio.py
@@ -4,13 +4,27 @@ Problem:
 """
 import pandas as pd
 from members_list import members_list
+from random_pair import random_pair
 
 # load members dataframe
 members = members_list()
 
-#
+# randomly pair members
+pairings = random_pair(members)
+pairings.reset_index(drop=True, inplace=True)
+ 
+print(pairings.to_string())
+print()
 
+# print giver of mine
+ready = input('If ready to see my giver, then enter y: ')
 
+while ready != 'y':
+	ready = input('If ready to see my giver, then enter y: ')
+	if ready == 'y':
+		break
 
+my_giver = pairings.loc[pairings['receiver'] == 'Alex']
 
-
+print('\n\nThe giver of Alex is:')
+print(my_giver.to_string())
